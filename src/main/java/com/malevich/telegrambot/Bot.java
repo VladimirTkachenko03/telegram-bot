55d744fa21f64e7e189c9b866d2145a1f6f1dd23
Vladimir Tkachenko <tka4enko.vova03@gmail.com>
2022-06-20 15:28:29 +0300

    Create method with keyboard and buttons.

--- src/main/java/com/malevich/telegrambot/Bot.java
+++ src/main/java/com/malevich/telegrambot/Bot.java
@@ -5,10 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 @Component
 public class Bot extends TelegramLongPollingBot {
@@ -34,7 +32,7 @@ public class Bot extends TelegramLongPollingBot {
         if (update.hasMessage()) {
             Message message = update.getMessage();
             if (message.hasText()) {
-                sendMessageService.test(message);
+                sendMessageService.keyboardTest(message);
             }
         }
     }
--- src/main/java/com/malevich/telegrambot/services/SendMessageService.java
+++ src/main/java/com/malevich/telegrambot/services/SendMessageService.java
@@ -4,6 +4,11 @@ import com.malevich.telegrambot.messagesender.MessageSender;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
+
+import java.util.ArrayList;
 
 @Service
 public class SendMessageService {
@@ -14,7 +19,7 @@ public class SendMessageService {
         this.messageSender = messageSender;
     }
 
-    public void test(Message message) {
+    public void testStyle(Message message) {
         SendMessage message1 = SendMessage.builder()
                 .text("<b> Bold </b>" +
                         "<i> italic </i>" +
@@ -25,5 +30,39 @@ public class SendMessageService {
                 .build();
 
         messageSender.sendMessage(message1);
+    }
+
+    public void keyboardTest (Message message) {
+        ReplyKeyboardMarkup markup = new ReplyKeyboardMarkup();
+        ArrayList<KeyboardRow> keyboardRows = new ArrayList<KeyboardRow>();
+        KeyboardRow rowButtons = new KeyboardRow();
+        KeyboardRow rowContact = new KeyboardRow();
+        KeyboardRow rowLocation = new KeyboardRow();
+
+
+        rowButtons.add("Button1");
+        rowButtons.add("Button2");
+        rowButtons.add("Button3");
+
+        rowContact.add(KeyboardButton.builder().text("Phone Number ")
+                .requestContact(true)
+                .build());
+
+        rowLocation.add(KeyboardButton.builder()
+                .requestLocation(true)
+                .text("Location ")
+                .build());
+        keyboardRows.add(rowButtons);
+        keyboardRows.add(rowContact);
+        keyboardRows.add(rowLocation);
+
+        markup.setKeyboard(keyboardRows);
+        markup.setResizeKeyboard(true);
+
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setText("Test");
+        sendMessage.setChatId(String.valueOf(message.getChatId()));
+        sendMessage.setReplyMarkup(markup);
+        messageSender.sendMessage(sendMessage);
     }
 }
